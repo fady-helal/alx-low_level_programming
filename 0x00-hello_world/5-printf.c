2806b39449dca5d93c384350d7cd1e85fb2080ed
fady-helal <fadyhelal8@gmail.com>
2023-09-07 13:05:17 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-/** 
+
+/**
  * main - Entry point
- *  
- * Description :print text by printf
  *
- * return :always 0(success)
+ * Description: use puts to print
+ *
+ * Return: Always 0 (Success)
 */
+
 int main(void){
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
