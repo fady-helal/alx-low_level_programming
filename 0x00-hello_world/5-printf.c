2d5862d9e8a7d8910bd6eb3f1711ff367dab7d3b
fady-helal <fadyhelal8@gmail.com>
2023-09-07 09:38:18 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/** 
+ * main -entry point 
+ * describtion :print text by printf
+*/
+
+
 int main(void){
 printf("with proper grammar, but the outcome is a piece of art");
 return (0);
