47de96e3ee2f6bfa8e2b688e67dd8b4b98132b13
fady-helal <fadyhelal8@gmail.com>
2023-09-07 12:47:17 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,12 @@
  * main - Entry point
  *
  * Description: use puts to print
+ *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
