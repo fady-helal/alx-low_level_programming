c93ba3636c4207ba8605ef257c21979e22edb08f
fady-helal <fadyhelal8@gmail.com>
2023-09-07 08:38:25 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-    puts("/"Programming is like building a multilingual puzzle");
+ puts ("\"git add . && git commit -m "update" && git push");
     return (0);
 }
