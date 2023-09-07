9616c820491109308e743b283cb49c0306162daa
fady-helal <fadyhelal8@gmail.com>
2023-09-07 08:11:28 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
-#include<stdio.h>
-int main (void){
+#include <stdio.h>
 
-
-puts ("Programming is like building a multilingual puzzle");
-	return 0;
-
-
-
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
 }
+
