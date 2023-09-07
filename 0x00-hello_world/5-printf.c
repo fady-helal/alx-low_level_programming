f4352232ec47470cf239a731212019fea1e2b5d8
fady-helal <fadyhelal8@gmail.com>
2023-09-07 12:53:26 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,10 @@
  * main - entry point
  *  
  * describtion :print text by printf
+ *
  * return :always 0(success)
 */
-int main(void){
-printf("with proper grammar, but the outcome is a piece of art.\n");
-return (0);
+	int main(void){
+	printf("with proper grammar, but the outcome is a piece of art.\n");
+	return (0);
 }
