ce445471e73515d374449088b045a44d48d486e4
fady-helal <fadyhelal8@gmail.com>
2023-09-07 12:55:35 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
  *
  * return :always 0(success)
 */
-	int main(void){
-	printf("with proper grammar, but the outcome is a piece of art.\n");
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
