b6709455f53d61c9e4aa33d17cd711447f8383c5
fady-helal <fadyhelal8@gmail.com>
2023-09-08 14:28:00 -0700

    update

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,12 @@
 */
 int main(void)
 {
-	char x;
-	for (x = 'a' ; x <= 'z' ; x++)
-	{
-	putchar(x);
-	}
-putchar('\n');
-	return (0);
+char x;
+for (x = 'a' ; x <= 'z' ; x++)
+{
+putchar(x);
 }
+putchar('\n');
+return (0);
+}
+
