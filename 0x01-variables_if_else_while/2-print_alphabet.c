c6e21d38a4f90f517f7f13e7ae7d021c5c5a4cbf
fady-helal <fadyhelal8@gmail.com>
2023-09-08 13:28:47 -0700

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * mian - Entry point
+ *
+ * Description : print lower alpabet
+ *
+ * return (0) :success
+*/
+int main(void)
+{
+
+	for (int i = 97 ; i <= 122 ; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+	return (0);
+}
