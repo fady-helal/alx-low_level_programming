cd92993593f7c98306b3a2cb78e588fe5921b8ef
fady-helal <fadyhelal8@gmail.com>
2023-09-09 02:28:13 -0700

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:expect letter e and q
+ *
+ * Return: (0)success
+*/
+int main (void)
+{
+char x;
+for (x = 'a' ; x <= 'z' ; x++)
+{
+	if ( x != 'e' && x != 'q')
+		{
+		putchar (x);
+		}
+}
+putchar ('\n');
+}
