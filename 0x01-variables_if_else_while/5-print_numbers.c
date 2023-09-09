b4df301a3746f231868a86f0c864468c0d158854
fady-helal <fadyhelal8@gmail.com>
2023-09-09 03:12:40 -0700

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit
+ *
+ * Return: (0)success
+*/
+int main(void)
+{
+for(int i = 0 ; i <= 9 ;i++)
+	pritnf ("%d",i);
+printf("\n");
+return (0);
+}
