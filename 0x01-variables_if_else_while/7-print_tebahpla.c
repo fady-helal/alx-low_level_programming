03cb17e4af1a8e527c4afb678b13e3f793c5126c
fady-helal <fadyhelal8@gmail.com>
2023-09-09 06:33:01 -0700

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main -Entry Point
+ *
+ * Description: for loop for print alphabit reversed
+ *
+ * Return: Alwyas 0 (Success)
+ */
+int main(void)
+{
+int x;
+for (x = 123 ; x < 97 ; x--)
+{
+	putchar('x');
+}
+putchar('\n');
+}
