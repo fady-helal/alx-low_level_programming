933ca2d180bec02ca189e20d5c02b56d203cdd65
fady-helal <fadyhelal8@gmail.com>
2023-09-09 06:39:15 -0700

    update

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 {
-char x;
-for (x = 'z' ; x >= 'a' ; x--)
-{
-	putchar('x');
-}
-putchar('\n');
-return (0);
+	char x;
+
+	for (x = 'z' ; x >= 'a' ; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
 }
