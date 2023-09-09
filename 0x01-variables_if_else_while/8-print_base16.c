023239f6583cb86f784298bc1b5e2c046c105e4f
fady-helal <fadyhelal8@gmail.com>
2023-09-09 13:21:03 -0700

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print Hex decimal
+ *
+ * Return: (0)success
+*/
+int main(void)
+{
+int x, y;
+for (x = 48 ; x < 58 ; x++)
+{
+	putchar(x);
+}
+for (y = 65 ; y < 71 ; y++)
+{
+	putchar(y);
+}
+	putchar('\n');
+return (0);
+}
+
