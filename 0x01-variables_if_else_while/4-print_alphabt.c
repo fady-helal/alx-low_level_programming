20cdf8e0af95b87d7209ec97f6acc39583575e1e
fady-helal <fadyhelal8@gmail.com>
2023-09-09 02:37:12 -0700

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,12 +6,12 @@
  *
  * Return: (0)success
 */
-int main (void)
+int main(void)
 {
 char x;
 for (x = 'a' ; x <= 'z' ; x++)
 {
-	if ( x != 'e' && x != 'q')
+	if ( x != 'e' && x != 'q' )
 		{
 		putchar (x);
 		}
