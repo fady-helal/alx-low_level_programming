e576eb96eea89f7072d89a35b8d48568ef9078b7
fady-helal <fadyhelal8@gmail.com>
2023-09-10 00:36:38 -0700

    update

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,27 +4,29 @@
  *
  * Description: nested loop
  *
- * Return: Always 0 (Success)
+ * Return: Alwyas 0 (Success)
  */
 int main(void)
 {
-int x, y, z;
-for (x = 0 ; x <= 9 ; x++)
-{
-	for (y = x + 1 ; y <= 9 ; y++)
+	int x, y, z;
+
+	for (x = 0 ; x <= 9 ; x++)
 	{
-	for (z = y + 1 ; y <= 9 ; y++)
+		for (y = x + 1 ; y <= 9 ; y++)
 		{
-	putchar (x + '0');
-	putchar (y + '0');
-	putchar (z + '0');
-	if (x == 7 && y == 8 && z == 9)
-	continue;
-	putchar (',');
-	putchar (' ');
+			for (z = y + 1 ; z <= 9 ; z++)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(z + '0');
+				if (x == 7 && y == 8 && z == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
+	putchar('\n');
+	return (0);
 }
-putchar ('\n');
-return (0);
-}
+
