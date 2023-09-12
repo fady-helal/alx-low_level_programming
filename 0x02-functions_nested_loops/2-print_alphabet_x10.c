5e24ac36c82c46a4db5201af1a038f2a983a0e5b
fady-helal <fadyhelal8@gmail.com>
2023-09-12 13:10:22 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,13 @@
 */
 void print_alphabet_x10(void)
 {
-	char = i;
+	int = i;
 	int = j;
-	for (i = 'a' ; i <= 'z' ;z++)
+	for (j = 0 ; j <= 9 ;j++)
 	{
-		for (j = 0 ;j <= 9 ;j++ )
+		for (i = 'a' ; i <= 'z' ;z++)
 		{
-			_putchar (a);
+			_putchar (i);
 		}
 		_putchar ('\n');
 	}
