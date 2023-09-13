5bc4502427ee3ca314cdced4c87bf074f076df86
fady-helal <fadyhelal8@gmail.com>
2023-09-13 16:22:19 -0700

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * add - add numbers
+ * @a: parameter of int
+ * @b: int para
+ *
+ * Description: adding
+ *
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	int x = a + b;
+
+	return (x);
+}
