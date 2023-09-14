075bf2e720c54d90cf1ce2e0e0ac33da021336bc
fady-helal <fadyhelal8@gmail.com>
2023-09-14 16:22:54 -0700

    update

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,12 @@
 /**
  * multiplies two integers.
  * return (0)
- *
- *
+ * a is a parameter
+ * b is a second parameter
 */
 int mul(int a, int b)
 {
-	int c;
-	c = a * b;
-	return (c);
+int c;
+c = a * b;
+return (c);
 }
-	
