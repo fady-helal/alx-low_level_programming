7ef6aab581fa076a507efd83913781c1dfaca553
fady-helal <fadyhelal8@gmail.com>
2023-09-17 02:17:59 -0700

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,42 +8,14 @@
 
 void print_number(int n)
 {
-	unsigned int i;
-	int x, y, z, a;
-
+	unsigned int i = n;
 	if (n < 0)
 	{
-		i = n * -1;
 		_putchar('-');
+		i = -n;
 	}
-	else
-	{
-		i = n;
-	}
-	x = i % 10;
-	y = ((i % 100) - x) / 10;
-	z = (i % 1000 - i % 100) / 100;
-	a = (i - (i % 1000)) / 1000;
-
-	if ((n >= 0 && n <= 9) || (n < 0 && n >= -9))
-	{
-		_putchar(i + '0');
-	}
-	else if ((n > 9 && n <= 99) || (n < -9 && n >= -99))
-	{
-		_putchar(y + '0');
-		_putchar(x + '0');
-	}
-	else if ((n > 99 && n <= 999) || (n < -99 && n >= -999))
-	{
-		_putchar(z + '0');
-		_putchar(y + '0');
-		_putchar(x + '0'); }
-	else if ((n > 999 && n <= 9999) || (n < -999 && n >= -9999))
-	{
-	_putchar(a + '0');
-	_putchar(z + '0');
-	_putchar(y + '0');
-	_putchar(x + '0');
-	}
+	if ((n /10) > 0)
+		print_number(i / 10);
+	_putchar((i % 10) + 48);
 }
+	
