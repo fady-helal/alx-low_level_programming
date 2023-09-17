ef556cf1d8c6569f62fae10e8814b2ef579a851f
fady-helal <fadyhelal8@gmail.com>
2023-09-17 02:40:59 -0700

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,17 +4,16 @@
  * print_number - printing numbers with putchar
  * @n: intger number
  * Return: no return
- */
-
+*/
 void print_number(int n)
 {
 	unsigned int i = n;
-	if (n < 0)
-	{
-		_putchar('-');
-		i = -i;
-	}
+if (n < 0)
+{
+	_putchar('-');
+	i = -i;
+}
 	if ((n / 10) > 0)
 		print_number(i / 10);
 	_putchar((i % 10) + 48);
-}	
+}
