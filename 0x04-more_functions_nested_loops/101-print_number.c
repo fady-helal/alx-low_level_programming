679eebcff4110cf2d7daf5c180a88afba9d6501c
fady-helal <fadyhelal8@gmail.com>
2023-09-17 02:20:37 -0700

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,10 +12,9 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		i = -n;
+		i = -i;
 	}
-	if ((n /10) > 0)
+	if ((n / 10) > 0)
 		print_number(i / 10);
 	_putchar((i % 10) + 48);
-}
-	
+}	
