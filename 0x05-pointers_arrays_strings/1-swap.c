6523cbd4077ee27ee901a773b5e27eb26a599244
fady-helal <fadyhelal8@gmail.com>
2023-09-18 14:31:20 -0700

    update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * description : swap two integer
+ * @a,@b is a pointer
+ * return : none
+ * swap_int swap two integer
+ * */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
+
