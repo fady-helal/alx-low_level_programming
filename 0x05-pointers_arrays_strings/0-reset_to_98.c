6fcf108b76d4a9c5fa6a91c5e8fcdcdddf625273
fady-helal <fadyhelal8@gmail.com>
2023-09-18 14:20:40 -0700

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset any var throw pointer
+ * Description: change any parametr throw pointer
+ * @n: pointer of var
+ * Return: none
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
