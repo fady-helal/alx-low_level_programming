e33c88d3e18d61f8f357c5dcd3275ed75ac40598
fady-helal <fadyhelal8@gmail.com>
2023-09-19 00:34:25 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print reversed array
+ * @s: string Array
+ * Return: void none
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int length;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	length = i - 1;
+	while (length >= 0)
+	{
+		_putchar(s[length]);
+		length--;
+	}
+	_putchar('\n');
+}
