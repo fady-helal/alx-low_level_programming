f756a9169b5d7fa6c74a0c19b006a130ae17a753
fady-helal <fadyhelal8@gmail.com>
2023-09-18 23:40:53 -0700

    update

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - print string array into stdout
+ * @str: string array
+ * Return: none
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
