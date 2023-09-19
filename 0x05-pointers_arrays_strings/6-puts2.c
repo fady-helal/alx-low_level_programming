86ac1418e74a7f3afce1bd73da0522c99be384f6
fady-helal <fadyhelal8@gmail.com>
2023-09-19 01:54:28 -0700

    update

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts2 - print n%%2==0
+ * @str: array
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = *str;
+	int length = 0;
+while (str[length] != '\n')
+{
+length++;
+
+}
+
+	for (i = 0 ; i < length ; i++)
+	{
+	if (i % 2 == 0 )
+	{
+		_putchar(i + '0');
+	}
+	}
+	_putchar('\n');
+}
