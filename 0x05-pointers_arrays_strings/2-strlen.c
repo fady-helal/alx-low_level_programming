4fb63a23632b5c7c7c92ce9906b24ea9fc64c3cb
fady-helal <fadyhelal8@gmail.com>
2023-09-18 23:04:13 -0700

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen : calculate number of string
+ * @s : is a pointer
+ *
+ * return : i
+*/
+int _strlen(char *s)
+{
+
+int i = 0;
+while (*s != '\0')
+{
+	i++;
+}
+return (i);
+}
