f2ae78907a8fa15ee6f4236aca755dc401388058
fady-helal <fadyhelal8@gmail.com>
2023-09-20 14:27:03 -0700

    update

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse element of array
+ * @a: array str
+ * @n: size of elemetns
+ * Return: none (void)
+ */
+
+void reverse_array(int *a, int n)
+{
+	int end, start ,temp ;
+	end = n - 1;
+	while (start < end)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+		start++;
+		end--;
+	}
+}
