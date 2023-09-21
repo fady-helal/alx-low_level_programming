9f71600cec8a920a11022b4850bc92810acc8085
fady-helal <fadyhelal78>
2023-09-21 16:36:30 +0300

    update

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,41 @@
 #include "main.h"
 
 /**
- * is_sep - function to check separetor
- * @c: char
- * Return: 1 if sep 0 if not
+ *  * is_sep - function to check separetor
+ *   * @c: char
+ *    * Return: 1 if sep 0 if not
  */
 
 int is_sep(char c)
 {
-	int i;
-	char *str = " \t\n,;.!?\"(){}";
+int i;
+char *str = " \t\n,;.!?\"(){}";
 
-	for (i = 0; str[i] != '\0' ; i++)
-	{
-		if (c == str[i])
-			return (1);
-	}
-	return (0);
+for (i = 0; str[i] != '\0' ; i++)
+{
+if (c == str[i])
+return (1);
+}
+return (0);
 }
 
 /**
- * cap_string - captilize first letter of words
- * @str: string array
- * Return: char *
+ *  * cap_string - captilize first letter of words
+ *   * @str: string array
+ *    * Return: char *
  */
+
 char *cap_string(char *str)
 {
-	int i;
+int i;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
-			str[i] = str[i] - 32;
-		if (str[i] >= 'a' && str[i] <= 'z' && (is_sep(str[i - 1])))
-			str[i] = str[i] - 32;
-	}
-	str[i] = '\0';
-	return (str);
+for (i = 0 ; str[i] != '\0' ; i++)
+{
+	if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+	str[i] = str[i] - 32;
+	if (str[i] >= 'a' && str[i] <= 'z' && (is_sep(str[i - 1])))
+	str[i] = str[i] - 32;
+}
+str[i] = '\0';
+return (str);
 }
