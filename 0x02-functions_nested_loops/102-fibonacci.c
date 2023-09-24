7de48978c67c4fb8381a522054fa0621673a4ae5
fady-helal <fadyhelal8@gmail.com>
2023-09-24 02:27:29 -0700

    update

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: fibonacci number
+ *
+ * Return: Alwyas 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int first,second,next ,i;
+	first = 0;
+	second = 1;
+	for(i = 0 ; i < 50 ; i++)
+	{
+		next = first + second ;
+		second = first ;
+		first = next;
+		printf("res = %d ",next);
+		if (i != 49)
+		{
+			printf(", ");
+		}
+		else 
+		{
+			printf("\n");
+		}
+	}
+}
