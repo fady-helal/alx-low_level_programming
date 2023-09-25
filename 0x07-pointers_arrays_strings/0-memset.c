545f271d20a2a0cc28772e5514e9a82d7e2b7b28
fady-helal <fadyhelal78>
2023-09-25 14:33:48 +0300

    update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  * _memset - function change the first bytes of string
- *   * @s: pointer to string array
- *    * @b: char
- *     * @n: int number of bytes
- *      * Return: char *
+ * * _memset - function change the first bytes of string
+ * * @s: pointer to string array
+ * * @b: char
+ * * @n: int number of bytes
+ * * Return: char *
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -13,6 +13,8 @@ char *_memset(char *s, char b, unsigned int n)
 		unsigned int i;
 
 			for (i = 0 ; i < n ; i++)
+			{
 						s[i] = b;
+			}
 				return (s);
 }
