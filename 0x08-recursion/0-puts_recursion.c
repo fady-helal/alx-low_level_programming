7aea7a58c6a46b75c9e1e636b9ad547e302ae91f
fady-helal <fadyhelal78>
2023-09-28 22:54:43 +0300

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
-
 /**
- * _puts_recursion - print each char recursiovly
- * @s: pointer to string
- * Return: void
- */
+ *  * _puts_recursion - print each char recursiovly
+ *   * @s: pointer to string
+ *    * Return: void
+ *     */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-{
-    _putchar('\n');
-    return;
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion (s + 1);
 }
-_putchar(*s);
-_puts_recursion (s + 1);
-}
+
