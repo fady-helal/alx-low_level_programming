1b7509dca8b10ea5cc9318f08bed5abf89195b7c
fady-helal <fadyhelal78>
2023-10-01 02:01:27 +0300

    update

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *  * _prime - found prime number
+ *   * @x: int
+ *    * @y: int
+ *     * Return: (1) if prime (0) if not
+ */
+
+int _prime(int x, int y)
+{
+		if (y == x)
+					return (1);
+			if (x % y == 0)
+						return (0);
+				return (_prime(x, y + 1));
+}
+
+/**
+ *  * is_prime_number - found if the number is prime or not
+ *   * @n: int
+ *    * Return: (1) if prime (0) if not
+ */
+
+int is_prime_number(int n)
+{
+		if (n <= 1)
+					return (0);
+			return (_prime(n, 2));
+}
