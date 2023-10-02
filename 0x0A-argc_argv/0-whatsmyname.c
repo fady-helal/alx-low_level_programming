8acd33d1ded8ea8d7c9b50ec1d437772cc674bef
fady-helal <fadyhelal78>
2023-10-02 17:46:41 +0300

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * @argc: number of args
- * @argv: 2d array of args
- * Return: Alwyas(0)
- */
+ *  main - Entry point
+ ** @argc: number of args
+ * * @argv: 2d array of args
+ ** Return: Alwyas(0)
+ **/
 
 int main(int argc, char *argv[])
 {
-	int x;
-
-	x = argc - 1;
-	printf("%s\n", argv[x]);
-
-	return (0);
+int x;
+x = argc - 1;
+printf("%s\n", argv[x]);
+return (0);
 }
