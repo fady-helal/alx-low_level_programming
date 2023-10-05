e01fa60d3a57399b5d45b2e3c8185039e570414e
fady-helal <fadyhelal78>
2023-10-06 01:44:52 +0300

    update

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *  * argstostr - function that turn every argments passed int string
+ *   * @ac: number of args
+ *    * @av: 2d pointer to pointer
+ *     * Return: pointer to str
+ *      */
+
+char *argstostr(int ac, char **av)
+{
+int i, j, size, x;
+char *str;
+
+if (ac == 0 || av == NULL)
+return (NULL);
+
+size = 0;
+for (i = 0 ; i < ac ; i++)
+{
+for (j = 0 ; av[i][j] != '\0' ; j++)
+size++;
+size++;
+}
+size++;
+
+str = malloc(sizeof(char) * size);
+if (str == NULL)
+return (NULL);
+
+x = 0;
+for (i = 0 ; i < ac ; i++)
+{
+for (j = 0 ; av[i][j] != '\0' ; j++)
+str[x++] = av[i][j];
+str[x++] = '\n';
+}
+str[x] = '\0';
+return (str);
+}
