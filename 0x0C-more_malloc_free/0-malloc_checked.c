30de28dade367452a3fdd06768a37aab0f5bfb66
fady-helal <fadyhelal78>
2023-10-08 09:49:32 +0300

    update

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * malloc_checked - return pointer to allocated memo
+ * @b: byte
+ * Return: void pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+
+void *new;
+new = malloc(b);
+if (new == NULL)
+{
+    exit (98);
+}
+return (new);
+
+
+
+}
