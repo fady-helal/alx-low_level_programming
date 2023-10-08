7dbda2f588f8d5d5949fc9cf9232c514cd383ec7
fady-helal <fadyhelal78>
2023-10-08 09:54:25 +0300

    update

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,11 +8,11 @@ void *malloc_checked(unsigned int b)
 {
 
 	void *new;
-	new = malloc(b);
-	if (new == NULL)
-	{
-		exit (98);
-	}
+new = malloc(b);
+if (new == NULL)
+{
+	exit(98);
+}
 	return (new);
 
 
