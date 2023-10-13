522d5ab2b33830e770e031f0472929f4bf608459
fady-helal <fadyhelal78>
2023-10-14 01:32:07 +0300

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,14 @@
  *  * print_name - function that take pointer to function and char * as its par
  *   * @name: pointer to char
  *    * @f: pointer to function
- *     * Return: NONE
+ * Return: NONE
  */
 
 void print_name(char *name, void(*f)(char *))
 {
 if (!name || !f)
 return;
+
 f(name);
+
 }
