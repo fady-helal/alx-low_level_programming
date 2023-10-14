1502b30d4298b48ae3601a8b093f251caded1f04
fady-helal <fadyhelal78>
2023-10-14 19:01:40 +0300

    update

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *  * main - entry point
+ *   * @argc: number of argments
+ *    * @argv: 2d array
+ *     * Return: Alwyas (0)
+ *      */
+
+int main(int argc, char *argv[])
+{
+		int (*f)(int, int);
+			int res;
+
+				if (argc != 4)
+						{
+									printf("Error\n");
+											exit(98);
+												}
+					if ((argv[2][0] == '/' || argv[2][0] == '%') && (atoi(argv[3]) == 0))
+							{
+										printf("Error\n");
+												exit(100);
+													}
+						f = get_op_func(argv[2]);
+							if (f == NULL)
+									{
+												printf("Error\n");
+														exit(99);
+															}
+								res = f(atoi(argv[1]), atoi(argv[3]));
+									printf("%d\n", res);
+
+										return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ *  * op_add - add two int
+ *   * @a: int
+ *    * @b: int
+ *     * Return: int
+ *      */
+int op_add(int a, int b)
+{
+		return (a + b);
+}
+
+/**
+ *  * op_sub - sub two int
+ *   * @a: int
+ *    * @b: int
+ *     * Return: int
+ *      */
+int op_sub(int a, int b)
+{
+		return (a - b);
+}
+
+/**
+ *  * op_mul - mul two numbers
+ *   * @a: int
+ *    * @b: int
+ *     * Return: int
+ *      */
+int op_mul(int a, int b)
+{
+		return (a * b);
+}
+
+/**
+ *  * op_div - div two int
+ *   * @a: int
+ *    * @b: int
+ *     * Return: int
+ *      */
+int op_div(int a, int b)
+{
+		return (a / b);
+}
+
+/**
+ *  * op_mod - reminder of two int
+ *   * @a: int
+ *    * @b: int
+ *     * Return: int
+ *      */
+int op_mod(int a, int b)
+{
+		return (a % b);
+}
