f20f2ab6be2bf27065bd86f1c5712870c3849a9c
fady-helal <fadyhelal78>
2023-10-15 17:15:06 +0300

    update

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ *  * init_dog - function for new struct data of dog
+ *   * @d: struct dog pointer
+ *    * @name: name of th dog char*
+ *     * @age: float
+ *      * @owner: owner name (char*)
+ *       * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+		if (d == 0)
+					return;
+			d->name = name;
+				(*d).age = age;
+					d->owner = owner;
+}
