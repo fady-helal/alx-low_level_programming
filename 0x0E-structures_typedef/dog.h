d3a6d1bf92717795abb9b69d835bf23280d6b164
fady-helal <fadyhelal78>
2023-10-15 16:54:48 +0300

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - dog data
+ * @name: pointer to char
+ * @age: float
+ * @owner: pointer to char
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+
+
+
+#endif
