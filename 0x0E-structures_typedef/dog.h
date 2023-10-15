bfc7f90ea4378861b0dd81953cf9848c49ad079c
fady-helal <fadyhelal78>
2023-10-15 17:53:27 +0300

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+
 /**
  * struct dog - dog data
  * @name: pointer to char
@@ -11,10 +13,10 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} my_dog;
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
