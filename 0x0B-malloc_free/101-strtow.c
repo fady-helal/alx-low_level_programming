7cf609c117df6e49116f4bcb14c504d0dca1f698
fady-helal <fadyhelal78>
2023-10-31 21:10:41 +0200

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,40 +1,73 @@
 #include "main.h"
-
 /**
- * argstostr - function that turn every argments passed int string
- * @ac: number of args
- * @av: 2d pointer to pointer
- ** Return: pointer to str
+ * _strlen - string len
+ * @s: pointer to str
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	if (!*s)
+		return (0);
+	return (1 + _strlen(s + 1));
+}
+/**
+ * count_word - count words in string
+ * @str: pointer to str
+ * Return: int
  */
 
-char *argstostr(int ac, char **av)
+int count_word(char *str)
 {
-int i, j, size, x;
-char *str;
+	int i, spc;
 
-if (ac == 0 || av == NULL)
-return (NULL);
-
-size = 0;
-for (i = 0 ; i < ac ; i++)
-{
-for (j = 0 ; av[i][j] != '\0' ; j++)
-size++;
-size++;
+	spc = 0;
+	for (i = 0 ; str[i] ; i++)
+	{
+		if (i && str[i] == ' ' && str[i - 1] != ' ')
+			spc++;
+	}
+	if (str[i] == '\0' && str[i - 1] != ' ')
+		spc++;
+	return (spc);
 }
-size++;
+/**
+ * strtow - string to words in 2d array
+ * @str: pointer to str
+ * Return: pointer to pointer
+ */
 
-str = malloc(sizeof(char) * size);
-if (str == NULL)
-return (NULL);
-
-x = 0;
-for (x = 0 ; x < ac ; x++)
+char **strtow(char *str)
 {
-for (j = 0 ; av[i][j] != '\0' ; j++)
-str[x++] = av[i][j];
-str[x++] = '\n';
-}
-str[x] = '\0';
-return (str);
+	char **new;
+	int words, i, j, count, count2;
+
+	if (str == NULL || count_word(str) == 0 || _strlen(str) == 0)
+		return (NULL);
+	words = count_word(str);
+	new = malloc(sizeof(char *) * (words + 1));
+	if (new == NULL)
+		return (NULL);
+
+	count = 0;
+	count2 = 0;
+	for (i = 0 ; i < words ; i++)
+	{
+		while (str[count] == ' ')
+			count++;
+		for (j = 0 ; str[count] != ' ' && str[count]; j++, count++)
+			;
+		new[i] = malloc(sizeof(char) * (j + 1));
+		if (new == NULL)
+		{
+			for (i = 0 ; i < words ; i++)
+				free(new[i]);
+			free(new);
+			return (NULL);
+		}
+		while (str[count2] == ' ')
+			count2++;
+		for (j = 0 ; str[count2] != ' ' && str[count2] ; j++, count2++)
+			new[i][j] = str[count2];
+	}
+	return (new);
 }
