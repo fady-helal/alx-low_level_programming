966da21c8cce2e286d72ec706ec98ed96eb31dc6
fady-helal <fadyhelal78>
2023-11-03 12:52:16 +0200

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,74 +1,114 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+
 /**
- *  *  _puts - print a string following a new line 
- *   * @str : pointer to a string to print
- *    * return : void
-*/
-void _puts(*str)
+ * _isdigit - checks if character is digit
+ * @c: the character to check
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
+int _isdigit(int c)
 {
-int i;
-for (i = 0 ; str[i] != '\0' ; i++)
-{
-_putchar(str);
-}
-_putchar('\n');
+	return (c >= '0' && c <= '9');
 }
 
 /**
- *  *  _atoi - convert a string to an integer 
- *   *  @s : char type string
- *    * Return : integer converted
- *    */
-int _atoi(char *s)
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
 {
-unsigned int  res = 0;
-int i = 0;
-int sign = 1;
+	int i = 0;
 
-if (s[0] == '-')
-{
-sign = -1;
-i++;
-}
-for (;s[i] != '\0' ; ++i)
-{
-res = res * 10 + s[i] - '0';
-}
-return (sign * res);
+	while (*s++)
+		i++;
+	return (i);
 }
 
 /**
- *  *  print_int : print an integer
- *   *  @n : int
- *    * Return : 0
- *    */
-void print_int(unsigned long int n)
+ * big_multiply - multiply two big number strings
+ * @s1: the first big number string
+ * @s2: the second big number string
+ *
+ * Return: the product big number string
+ */
+char *big_multiply(char *s1, char *s2)
 {
-unsigned long int divisor = 1, i, resp;
-for (i = 0 ; n / divisor > 9; i++ , divisor *=10)
-;
-for (; divisor >= 1 ; n %= divisor , divisor /= 10)
-{
-    resp = n / divisor;
-        _putchar(resp + '0');
-}
+	char *r;
+	int l1, l2, a, b, c, x;
+
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	r = malloc(a = x = l1 + l2);
+	if (!r)
+		printf("Error\n"), exit(98);
+	while (a--)
+		r[a] = 0;
+
+	for (l1--; l1 >= 0; l1--)
+	{
+		if (!_isdigit(s1[l1]))
+		{
+			free(r);
+			printf("Error\n"), exit(98);
+		}
+		a = s1[l1] - '0';
+		c = 0;
+
+		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+		{
+			if (!_isdigit(s2[l2]))
+			{
+				free(r);
+				printf("Error\n"), exit(98);
+			}
+			b = s2[l2] - '0';
+
+			c += r[l1 + l2 + 1] + (a * b);
+			r[l1 + l2 + 1] = c % 10;
+
+			c /= 10;
+		}
+		if (c)
+			r[l1 + l2 + 1] += c;
+	}
+	return (r);
 }
 
+
 /**
- *  *  main- print the result of the multiplication, follow by a new line
- *   * @argc: int
- *    * @argv: list
- *     * Return : 0
- *     */
-int main(int argc, const char *argv[])
+ * main - multiply two big number strings
+ * @argc: the number of arguments
+ * @argv: the argument vector
+ *
+ * Return: Always 0 on success.
+ */
+int main(int argc, char **argv)
 {
-(void)argc;
-if (argc != 3)
-{
-    _puts("Error ");
-        exit(98);
-}
-print_int(_atoi(argv[1]) * _atoi(argv[2]));
-_putchar('\n');
-return(0);
+	char *r;
+	int a, c, x;
+
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+
+	x = _strlen(argv[1]) + _strlen(argv[2]);
+	r = big_multiply(argv[1], argv[2]);
+	c = 0;
+	a = 0;
+	while (c < x)
+	{
+		if (r[c])
+			a = 1;
+		if (a)
+			_putchar(r[c] + '0');
+		c++;
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(r);
+	return (0);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,9 +8,9 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
-int main(int argc, char const *argv[]);
 void print_int(unsigned long int n);
-int _atoi(char *s);
-void _puts(*str);
-
+char *big_multiply(char *s1, char *s2);
+int main(int argc, char **argv);
+int _strlen(char *s);
+int _isdigit(int c);
 #endif
