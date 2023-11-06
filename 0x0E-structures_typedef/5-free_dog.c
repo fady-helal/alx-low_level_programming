39b27f37509e6243e714ce007bbb0afea05f6a02
fady-helal <fadyhelal78>
2023-11-07 00:19:19 +0200

    update

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,19 +1,19 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- *  * free_dog - free malloced memo
- *   * @d: struct dog
- *    * Return: NONE
- *     */
+ * free_dog - free malloced memo
+ * @d: struct dog
+ * Return: NONE
+ */
 
 void free_dog(dog_t *d)
 {
-		if (d != NULL)
-				{
-							if (d->name != NULL)
-											free(d->name);
-									if (d->owner != NULL)
-													free(d->owner);
-											free(d);
-												}
+if (d != NULL)
+{
+if (d->name != NULL)
+free(d->name);
+if (d->owner != NULL)
+free(d->owner);
+free(d);
+}
 }
