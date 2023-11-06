56668a5905ec0209074cf8a836e760e0a558256a
fady-helal <fadyhelal78>
2023-11-07 00:20:17 +0200

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,18 +8,18 @@
 
 char *_strcpy(char *s)
 {
-		char *new;
-			int i, len;
+char *new;
+int i, len;
 
-				for (len = 0 ; s[len] ; len++)
-							;
-					new = malloc(sizeof(char) * (len + 1));
-						if (new == NULL)
-									return (NULL);
-							for (i = 0; i < len ; i++)
-										new[i] = s[i];
-								new[i] = '\0';
-									return (new);
+for (len = 0 ; s[len] ; len++)
+;
+new = malloc(sizeof(char) * (len + 1));
+if (new == NULL)
+return (NULL);
+for (i = 0; i < len ; i++)
+new[i] = s[i];
+new[i] = '\0';
+return (new);
 }
 /**
  *  * new_dog - create struct dog
@@ -31,24 +31,24 @@ char *_strcpy(char *s)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-		dog_t *new;
+dog_t *new;
 
-			new = malloc(sizeof(dog_t));
-				if (new == NULL)
-							return (NULL);
-					new->name = _strcpy(name);
-						if (new->name == NULL)
-								{
-											free(new);
-													return (NULL);
-														}
-							new->age = (age) ? age : 0;
-								new->owner = _strcpy(owner);
-									if (new->owner == NULL)
-											{
-														free(new->name);
-																free(new);
-																		return (NULL);
-																			}
-										return (new);
+new = malloc(sizeof(dog_t));
+if (new == NULL)
+return (NULL);
+new->name = _strcpy(name);
+if (new->name == NULL)
+{
+free(new);
+return (NULL);
+}
+new->age = (age) ? age : 0;
+new->owner = _strcpy(owner);
+if (new->owner == NULL)
+{
+free(new->name);
+free(new);
+return (NULL);
+}
+return (new);
 }
