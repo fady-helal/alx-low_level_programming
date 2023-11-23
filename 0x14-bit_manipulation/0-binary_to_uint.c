ae04691a4b37535ad0bce1e487ad1e677c35535d
fady-helal <fadyhelal78>
2023-11-24 01:14:36 +0200

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,14 +4,14 @@ unsigned int binary_to_uint(const char *b)
 int i;
 unsigned int result = 0;
 int length;
-for (length = 0 ; b[length] != '\0' ; length++);
-
+for (length = 0 ; b[length] != '\0' ; length++)
+;
 for (i = 0 ; i < length ; i++)
 {
 if (b[i] == '1')
 {
-    result += (1 << (length - i - 1));
+result += (1 << (length - i - 1));
 }
 }
-return result;
+return (result);
 }
