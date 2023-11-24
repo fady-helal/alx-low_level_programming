c954730b2ff18f9a6538cecaf036d538862cdc68
fady-helal <fadyhelal78>
2023-11-24 23:54:38 +0200

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,13 +4,12 @@
  * @n:the number to index.
  * @index:the bit to set.
  * Return : 1 if success.
- * 
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 if (index >= sizeof(n) * 8)
 return (-1);
 if (*n & 1L << index)
-*n ^= 1L << index ;
+*n ^= 1L << index;
 return (1);
 }
