fbb19a64201a5243d684166f110333b361080feb
fady-helal <fadyhelal78>
2023-11-25 10:34:00 +0200

    update

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,20 +5,19 @@
  * @n:first number.
  * @m:second number.
  * Rerurn: number of bit should be flip
- * 
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned long int res_xor = n ^ m;
 unsigned int count = 0;
-while(res_xor)
+while (res_xor)
 {
-    if (res_xor & 1ul)
-    {
-        res_xor >> 1;
-        count++;
-    }
+if (res_xor & 1ul)
+{
+res_xor >> 1;
+count++;
+}
 }
 
-return(count);
+return (count);
 }
