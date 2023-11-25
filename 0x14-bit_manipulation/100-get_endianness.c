fec58addbca2d23f8eb3bb6e7bd1964a25d6f758
fady-helal <fadyhelal78>
2023-11-25 11:28:19 +0200

    update

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ *get_endianness - return the endianness of the system.
+ *Return : 1 if big endian , 0 if small.
+ */
+int get_endianness(void)
+{
+unsigned long int n = 1;
+return(* (char *) & n);
+}
+
+
