5366164a83360e63a57f280c27bf20d6757412f5
fady-helal <fadyhelal78>
2023-11-26 14:35:53 +0200

    update

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,10 +1,11 @@
 #include"lists.h"
 
 /**
- * 
- * 
-*/
-size_t list_len
+  * list_len - Show the number of elements of a list
+  * @h: A linked list
+  *
+  * Return: The number of elements of a list
+  */
 size_t list_len(const list_t *h)
 {
 size_t count = 0;
